8ea47c3fc77bba46e6ec342d16e1610b21919355
it-seekers <ricradotorres@gmail.com>
2024-07-18 12:27:36 -0600

    primer comit

---
+++ ITSLocation.API/ITSLocation.API.UnitTesting/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace ITSLocation.API.UnitTesting;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# ITSLocation_RicardoTorres
